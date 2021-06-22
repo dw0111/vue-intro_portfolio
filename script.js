600216d8fa18faaa243fa0629b3ad3a97f27049a
Daniel Wiesner <daniel_wiesner@gmx.de>
2021-06-22 09:37:58 +0200

    Add prettier

--- script.js
+++ script.js
@@ -1,2 +1,71 @@
-Vue.createApp().mount("header")
-Vue.createApp().mount("#blog")
+const links = [
+  {
+    id: 1,
+    name: 'home',
+    url: 'index.html',
+  },
+  {
+    id: 2,
+    name: 'portfolio',
+    url: 'portfolio.html',
+  },
+  {
+    id: 3,
+    name: 'contact me',
+    url: 'contact.html',
+  },
+]
+
+const posts = [
+  {
+    id: 1,
+    title: 'Why I learned Vue',
+    body: `I'm baby chambray street art <strong>thundercats</strong> occupy four loko
+        church-key disrupt. Shaman neutra bushwick chicharrones, tousled
+        air plant lomo williamsburg. Listicle aesthetic whatever prism,
+        ennui glossier asymmetrical scenester austin intelligentsia
+        cronut raw denim umami mumblecore. Lo-fi meh austin, selfies
+        hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore
+        tumeric.`,
+  },
+  {
+    id: 2,
+    title: 'Using the Vue CDN',
+    body: `I'm baby chambray street art <strong>thundercats</strong> occupy four loko
+        church-key disrupt. Shaman neutra bushwick chicharrones, tousled
+        air plant lomo williamsburg. Listicle aesthetic whatever prism,
+        ennui glossier asymmetrical scenester austin intelligentsia
+        cronut raw denim umami mumblecore. Lo-fi meh austin, selfies
+        hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore
+        tumeric.`,
+  },
+  {
+    id: 3,
+    title: 'How I mastered Vue',
+    body: `I'm baby chambray street art <strong>thundercats</strong> occupy four loko
+        church-key disrupt. Shaman neutra bushwick chicharrones, tousled
+        air plant lomo williamsburg. Listicle aesthetic whatever prism,
+        ennui glossier asymmetrical scenester austin intelligentsia
+        cronut raw denim umami mumblecore. Lo-fi meh austin, selfies
+        hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore
+        tumeric.`,
+  },
+]
+
+Vue.createApp({
+  beforeCreate() {},
+  created() {},
+  data() {
+    return {
+      name: 'Daniel',
+      links,
+      posts,
+      darkModeSet: true,
+    }
+  },
+  methods: {
+    toggleDarkMode() {
+      this.darkModeSet = !this.darkModeSet
+    },
+  },
+}).mount('body')
